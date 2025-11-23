3e09d3036c4ce74fe892dc0308c9861a8db2edbd
Ddumba Abdallah Kato <abdallahddumbakato@gmail.com>
2025-11-23 18:27:32 +0300

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -28,8 +28,10 @@ const __dirname = path.dirname(__filename);
 // Create transporter
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
+  // port: process.env.EMAIL_PORT,
+  // secure: false,
+  port: 465, // ← Change from 587 to 465
+  secure: true, // ← Change from false to true
   requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
